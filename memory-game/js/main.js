de407962f1c5c711155f752392edd8e123b86496
Kyle Burke <burke@DESKTOP-C7VI18Q.COM>
2020-04-06 13:41:11 -0400

    create game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,17 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+let cardOne = cards [0];
+cardsInPlay.push("cardOne");
+console.log("User Flipped Queen");
+let cardTwo = cards [2];
+cardsInPlay.push("cardTwo");
+console.log("User Flipped King");
+
+if (cardsInPlay.length === 2); {
+} else if (cardsInPlay[0] === cardsInPlay[1]); {
+	alert("You Found a Match");
+} else (cardsInPlay[0] === cardsInPlay[1]); {
+	alert("Sorry, try again.");
+}
+
